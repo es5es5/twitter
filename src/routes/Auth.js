0603963ebe61c87a410938df4372934507531ad7
es5es5 <es5e5s@nate.com>
2020-09-23 22:22:06 +0900

    2.1 Login form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 
 export default class Auth extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+  onChange = (event) => {
+    const { name, value } = event.target
+
+    switch (name) {
+      case 'email':
+        this.setState({
+          email: value
+        })
+        break
+      case 'password':
+        this.setState({
+          email: value
+        })
+        break
+      default:
+        break
+    }
+  }
+  onSubmit = (event) => {
+    console.log(event.target.value)
+  }
   render() {
     return (
       <div>
         <form>
-          <input type="text" placeholder="E-mail" required />
-          <input type="password" placeholder="Password" required />
+          <input type="text" name="email" placeholder="E-mail" value={this.state.email} required onChange={this.onChange} />
+          <input type="password" name="password" placeholder="Password" value={this.state.password} required />
         </form>
         <div>
           <button type="submit">GOOGLE</button>
