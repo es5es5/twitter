f15bde49d523acae5afd42969010d73d32a8accc
es5es5 <es5e5s@nate.com>
2020-09-29 23:17:47 +0900

    Navigation Comp

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
+import Navigation from 'components/Navigation';
 
 const AppRouter = ({isLoggedIn}) => {
   // eslint-disable-next-line
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {
         isLoggedIn ?
