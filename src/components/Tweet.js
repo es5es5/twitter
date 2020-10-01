3c4b1f9283da21c4b7adff1a93e49273e86aa792
es5es5 <es5e5s@nate.com>
2020-10-01 20:50:41 +0900

    3.5 Delete and Update

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,20 +1,70 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { dbService } from 'fbase'
 
 export default class Tweet extends Component {
-  static propTypes = {
-    tweetObj: Object,
-    isOwner: Boolean
+  state = {
+    editing: false,
+    newTweet: this.props.tweetObj.text
+  }
+
+  toggleEditing = () => {
+    this.setState({
+      editing: !this.state.editing
+    })
+  }
+
+  onDeleteClick = async () => {
+    const ok = window.confirm('Delete this Tweet?')
+    if (ok) {
+      await dbService.doc(`tweets/${this.props.tweetObj.id}`).delete()
+    }
+  }
+
+  onSubmit = async event => {
+    event.preventDefault()
+    await dbService.doc(`tweets/${this.props.tweetObj.id}`).update({
+      text: this.state.newTweet
+    })
+    this.setState({
+      editing: false
+    })
+  }
+  onChange = event => {
+    const {name, value} = event.target
+
+    switch (name) {
+      case 'newTweet':
+        this.setState({
+          newTweet: value
+        })
+      default:
+        break
+    }
   }
 
   render() {
     return (
       <div>
-        {this.props.tweetObj.text}
+        {
+          this.state.editing
+          ?
+          <>
+          <form onSubmit={this.onSubmit}>
+            <input type="text" name="newTweet" value={this.state.newTweet} required onChange={this.onChange} />
+            <button type="submit">EDIT</button>
+          </form>
+          <button type="button" onClick={this.toggleEditing}>CANCEL</button>
+          </>
+          :
+          <>
+          <h4>{this.props.tweetObj.text}</h4>
+          </>
+        }
         {this.props.isOwner &&
         <>
-          <button type="button">DELETE</button>
-          <button type="button">EDIT</button>
+          <button type="button" onClick={this.onDeleteClick}>DELETE</button>
+          <button type="button" onClick={this.toggleEditing}>EDIT</button>
         </>
         }
       </div>
